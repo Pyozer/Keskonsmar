a968ed642fbe87d04ebe10394768c5ccfc9265f2
OzakDevolo <quentin.louet@gmail.com>
2017-08-22 12:47:05 +0200

    Oui

---
+++ app/src/main/java/com/pyozer/keskonsmar/AddJdmActivity.java
@@ -0,0 +1,13 @@
+package com.pyozer.keskonsmar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddJdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_jdm);
+    }
+}
