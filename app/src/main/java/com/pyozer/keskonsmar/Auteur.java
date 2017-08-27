d4ed8fd3dda5cce4b47057fe04aacedbc3158693
OzakDevolo <quentin.louet@gmail.com>
2017-08-27 11:23:09 +0200

    Degrés d'auteur

--- app/src/main/java/com/pyozer/keskonsmar/Auteur.java
+++ app/src/main/java/com/pyozer/keskonsmar/Auteur.java
@@ -4,12 +4,22 @@ public class Auteur {
 
     private int id_user;
     private String pseudo_user;
+    private int  nb_tendance = 0;
+    private int nb_nul = 0;
 
     public Auteur(int id_user, String pseudo_user) {
 
         this.id_user = id_user;
         this.pseudo_user = pseudo_user;
     }
+
+    public void addTendance() { this.nb_tendance++; }
+
+    public int getNb_tendance() { return nb_tendance;}
+
+    public void addNul() { this.nb_nul++ ;}
+
+    public int getNb_nul() { return nb_nul;}
 
     public int getIdUser() {
         return id_user;
@@ -29,5 +39,9 @@ public class Auteur {
 
     public String toString() {
         return "@" + getPseudoUser();
+    }
+
+    public int getDegreAuteur() {
+        return getNb_tendance() - getNb_nul();
     }
 }
